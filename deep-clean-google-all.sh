99def033e1855c16921d1c9665c36aaeaea56a40
Ishan Dutta <ishandutta2007@MacBook-Pro.local>
2019-09-20 07:10:18 +0530

    google all

---
+++ deep-clean-google-all.sh
@@ -0,0 +1,11 @@
+rm -rf /Applications/Google*
+rm -rf ~/Library/Application\ Support/Google*
+rm -rf ~/Library/Application\ Support/CrashReporter/Google*
+rm -rf ~/Library/Preferences/com.google*
+rm -rf ~/Library/Preferences/Google*
+rm -rf ~/Library/Caches/com.google*
+rm -rf ~/Library/Saved\ Application\ State/com.google*
+rm -rf ~/Library/Google*
+rm -rf ~/Library/Google/Google*
+rm -rf ~/Library/Speech/Speakable\ Items/Application\ Speakable\ Items/Google*
+
